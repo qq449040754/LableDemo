199dd76dacffc7d8fb38e082a5295783ac086cec
qq449040754 <yhjavaee@163.com>
2013-01-27 11:17:55 +0800

    TestDome

--- LableDemo/ViewController.m
+++ LableDemo/ViewController.m
@@ -18,6 +18,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSString *s=@"字符串大小";
+    UIFont *font=[UIFont fontWithName:@"Arial" size:50.0f];
+    CGSize size=CGSizeMake(320, 2000);
+    UILabel *lable2= [[UILabel alloc ]initWithFrame:CGRectZero];
+    [lable2 setNumberOfLines:0];
+    CGSize lableSize=[s sizeWithFont:font
+                   constrainedToSize:size lineBreakMode:UILineBreakModeCharacterWrap];
+    lable2.frame=CGRectMake(0, 0, lableSize.width, lableSize.height);
+    lable2.textColor=[UIColor blackColor];
+    lable2.font=font;
+    lable2.text=s;
+    [self.view addSubview:lable2];
+
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +41,4 @@
 }
 
 @end
+ 
